6ffa91d3480af75519faf47b7afbaa676f206dfa
Kenji Phang <ken.pcx@outlook.com>
2024-10-24 23:55:19 +0100

    Moved mishal's code

--- components/ProblemGrid.tsx
+++ components/ProblemGrid.tsx
@@ -1,16 +1,26 @@
-import { Problem } from '@/data/mockProblems';
+import React from 'react';
 import ProblemCard from './ProblemCard';
+
+interface Problem {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  difficulty: string;
+}
 
 interface ProblemGridProps {
   problems: Problem[];
 }
 
-export default function ProblemGrid({ problems }: ProblemGridProps) {
+const ProblemGrid = ({ problems }: ProblemGridProps) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {problems.map((problem) => (
-        <ProblemCard key={problem.id} problem={problem} />
+        <ProblemCard key={problem.id} {...problem} />
       ))}
     </div>
   );
-}
+};
+
+export default ProblemGrid;
